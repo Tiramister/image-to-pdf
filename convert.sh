061d058a7c1a0f7226c9a19d0d3cf0b68af6f915
Tiramister <nh111424@gmail.com>
2024-02-23 16:38:12 +0900

    修正

--- convert.sh
+++ convert.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <dir>"
-  exit 1
+	echo "Usage: $0 <dir>"
+	exit 1
 fi
 
+# glob で no such file or directory が出ないようにする
+shopt -s nullglob
+
 dir=$1
+tmpdir="$dir/tmp"
 
 for imgdir in "$dir"/*/; do
-  dirname=$(basename "$imgdir")
-  echo "$dirname"
+	mkdir -p "$tmpdir"
 
-  convert "$imgdir"*.{png,jpeg,jpg} tmp.pdf
-  gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dPDFSETTINGS=/prepress -sOutputFile="$dir/$dirname.pdf" tmp.pdf
-  rm tmp.pdf
+	dirname=$(basename "$imgdir")
+	echo "convert directory: $dirname" >&2
+
+	# 同じサイズ・解像度の jpg に変換
+	for img in "$imgdir"*.{png,jpeg,jpg}; do
+		filename=$(basename "$img")
+		imgname=${filename%.*}
+
+		echo $imgname >&2
+		convert -resize 800x -units PixelsPerInch -density 96 "$img" "$tmpdir/$imgname.jpg"
+	done
+
+	convert "$tmpdir"/*.jpg "$dir/$dirname.pdf"
+
+	rm -r "$tmpdir"
 done
-
